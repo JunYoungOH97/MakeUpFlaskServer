c9577b5ac16f0c609f2a129e07aafa86f7035599
Oh-JunYoung <qawesdxc@naver.com>
2022-03-16 12:02:39 +0900

    [Fix] cleaning code of app.py

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask, request
-import requests
-
 from m_connection import s3_connection, s3_put_object, s3_get_image_path
-# from model import model
 from model.beautyGAN import beautyGAN
-
+from celery import Celery
 import keys
 
-from celery import Celery
-
 s3 = s3_connection()
+
 app = Flask(__name__)
 app.config.update(
     CELERY_BROKER_URL=f'redis://{keys.ip}:{keys.port}',
     CELERY_RESULT_BACKEND=f'redis://{keys.ip}:{keys.port}'
 )
+
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
@@ -25,36 +22,19 @@ def task(uuid):
 
     resultImage = beautyGAN(source, target).test()
     
-    ## /{uuid}/result/result.png
     result = f"{uuid}/result/result.png"
     ret = s3_put_object(s3, resultImage, result)
 
     return result, ret
 
 
-# @app.route('/')
 @app.route('/makeup', methods = ['POST'])
 def upload():
-    # response = requests.get("https://jsonplaceholder.typicode.com/users/1")
-    # source = s3_get_image_path(response.json()['source'])
-    # target = s3_get_image_path(response.json()['target'])
-    
-    # source = s3_get_image_path("KakaoTalk_Photo_2022-03-09-16-55-15")
-    # target = s3_get_image_path("KakaoTalk_Photo_2022-03-09-16-55-09")
-
     uuid = request.json["file"]
 
     result, ret = task(uuid)
 
-    if ret :
-        print("파일 저장 성공")
-        return result
-
-    else:
-        print("파일 저장 실패")
-        return "False"
-
-    # return s3_get_image_path(key_name)
+    return result if(ret) else "False"
 
 if(__name__ == "__main__"):
     app.run(host=keys.ip, port=keys.port, debug=True)
