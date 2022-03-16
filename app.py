5ed1850e9caa4e1a128e3b97f65ec34d58eb206a
Oh-JunYoung <qawesdxc@naver.com>
2022-03-16 11:00:34 +0900

    [Fix] app.py for asynchronous

--- app.py
+++ app.py
@@ -7,8 +7,30 @@ from model.beautyGAN import beautyGAN
 
 import keys
 
+from celery import Celery
+
 s3 = s3_connection()
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL=f'redis://{keys.ip}:{keys.port}',
+    CELERY_RESULT_BACKEND=f'redis://{keys.ip}:{keys.port}'
+)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@celery.task()
+def task(uuid):
+    source = s3_get_image_path(f"{uuid}/source/source.png")
+    target = s3_get_image_path(f"{uuid}/target/target.png")
+
+    resultImage = beautyGAN(source, target).test()
+    
+    ## /{uuid}/result/result.png
+    result = f"{uuid}/result/result.png"
+    ret = s3_put_object(s3, resultImage, result)
+
+    return result, ret
+
 
 # @app.route('/')
 @app.route('/makeup', methods = ['POST'])
@@ -22,14 +44,7 @@ def upload():
 
     uuid = request.json["file"]
 
-    source = s3_get_image_path(f"{uuid}/source/source.png")
-    target = s3_get_image_path(f"{uuid}/target/target.png")
-
-    resultImage = beautyGAN(source, target).test()
-    
-    ## /{uuid}/result/result.png
-    result = f"{uuid}/result/result.png"
-    ret = s3_put_object(s3, resultImage, result)
+    result, ret = task(uuid)
 
     if ret :
         print("파일 저장 성공")
